0f8d08e4e6ab3a68545df5f66a3b4097f689f871
Tanguy Le Cloirec <tanguy.lecloirec@student-cs.fr>
2024-12-21 16:59:00 +0100

    Problem 17

--- Problem 17.py
+++ Problem 17.py
@@ -9,7 +9,7 @@ num_to_letters = {
     5: 'five',
     6: 'six',
     7: 'seven',
-    8: 'heigth',
+    8: 'eight',
     9: 'nine',
     10: 'ten',
     11: 'eleven',
@@ -17,45 +17,54 @@ num_to_letters = {
     13: 'thirteen',
     14: 'fourteen',
     15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
 }
 
 dizaine = {
     0: '',
     1: 'teen',
-    2: 'twen',
-    3: 'thir',
-    4: 'for',
-    5: 'fif',
-    6: 'six',
-    7: 'seven',
-    8: 'height',
-    9: 'nine',
+    2: 'twenty',
+    3: 'thirty',
+    4: 'forty',
+    5: 'fifty',
+    6: 'sixty',
+    7: 'seventy',
+    8: 'eighty',
+    9: 'ninety',
 }
 
 def decomp(n):
     hundred = 0
     ten = 0
+    res = ''
     if n >= 100: # Then, there is hundred
-        hundred = int(str(n)[0])
+        hundred = n // 100
         n -= hundred*100 # We reduce n by the hundred to get only ten and units
+        if n == 0: #perfect hundred
+            res += num_to_letters[hundred] + 'hundred'
+        else:
+            res += num_to_letters[hundred] + 'hundred' + 'and'
+        
+    unit = n
     if n >= 10:
-        ten = int(str(n)[0])
-        n -= ten*10
-    unit = int(str(n)[0])
-    if hundred:
-        s = ""
-        if ten:
-            s = dizaine[ten] + 'ty'
-        return s + num_to_letters[unit] + 'and' + num_to_letters[hundred] + 'hundred' # 3 corresponds to 'and'
-    return num_to_letters[unit] + dizaine[ten] + 'ty'
+        ten = n // 10
+        unit = n % 10
+        if ten == 1:    
+            res += num_to_letters[unit + ten*10]
+        else:
+            res += dizaine[ten] + num_to_letters[unit]
+    else:
+        res += num_to_letters[unit]
+    return res
 
+print(decomp(888))
 count = 0
 for n in range(1, 1000):
-    if n < 16:
-        count += len(num_to_letters[n])
-    else:
-        count += len(decomp(n))
+    count += len(decomp(n))
       
     
-print(count + 11) # We add 'one thousand'
+print(count + 11) # We add 'onethousand'
 os.system("Pause")
